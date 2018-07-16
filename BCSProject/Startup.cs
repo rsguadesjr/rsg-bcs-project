8547dfe59a528aa128d19b9e091fb8a71f0ba1b3
Desiree Ann <Desiree Ann@EDDIE-C-DONS>
2018-07-16 22:25:34 +0800

    Add project files.

---
+++ BCSProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BCSProject.Startup))]
+namespace BCSProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
